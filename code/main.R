ff58de9e382415eb20076afeba6f336a283c7b12
Steeefan <sh@steeefan.de>
2017-02-21 17:49:55 -0500

    main.R: feature handling

--- code/main.R
+++ code/main.R
@@ -6,6 +6,7 @@ library(stringr)
 library(VIM)
 library(mice)
 library(lubridate)
+library(tidyr)
 
 # Read from JSON
 train = fromJSON(file = 'data/train.json')
@@ -28,6 +29,7 @@ trainDF = data.frame(
   interest_level = unlist(train$interest_level)
 )
 
+# Casting
 trainDF$description = as.character(trainDF$description)
 trainDF$display_address = as.character(trainDF$display_address)
 trainDF$street_address = as.character(trainDF$street_address)
@@ -101,16 +103,143 @@ trainDF = left_join(
 trainDF$featureCount = ifelse(is.na(trainDF$featureCount), 0, trainDF$featureCount)
 trainDF$photoCount = ifelse(is.na(trainDF$photoCount), 0, trainDF$photoCount)
 
+manager = left_join(
+  spread(
+    trainDF %>%
+      group_by(manager_id, interest_level) %>%
+      summarise(
+        n = n()
+      ),
 
-aggr(trainDF)
-aggr(trainDF, plot=F)
-md.pattern(trainDF)
+    key=interest_level,
+    value=n
+  ),
 
-saveRDS(trainDF, 'D:/train-v3.rds')
-write.csv(trainDF, 'D:/train-v3.csv', row.names=F)
+  trainDF %>% group_by(manager_id) %>%
+    summarise(
+      n = n()
+    ),
 
-saveRDS(features, 'D:/features-v2.rds')
-write.csv(features, 'D:/features-v2.csv', row.names=F)
+  by='manager_id'
+)
 
-saveRDS(photos, 'D:/photos-v2.rds')
-write.csv(photos, 'D:/photos-v2.csv', row.names=F)
+manager = transmute(
+  manager,
+  mgrHighPct = ifelse(is.na(round(high / n, 4)), 0, round(high / n, 4)),
+  mgrMediumPct = ifelse(is.na(round(medium / n, 4)), 0, round(medium / n, 4)),
+  mgrLowPct = ifelse(is.na(round(low / n, 4)), 0, round(low / n, 4))
+)
+
+
+building = left_join(
+  spread(
+    trainDF %>%
+      group_by(building_id, interest_level) %>%
+      summarise(
+        n = n()
+      ),
+
+    key=interest_level,
+    value=n
+  ),
+
+  trainDF %>% group_by(building_id) %>%
+    summarise(
+      n = n()
+    ),
+  by='building_id'
+)
+
+building = transmute(
+  building,
+  bldgHighPct = ifelse(is.na(round(high / n, 4)), 0, round(high / n, 4)),
+  bldgMediumPct = ifelse(is.na(round(medium / n, 4)), 0, round(medium / n, 4)),
+  bldgLowPct = ifelse(is.na(round(low / n, 4)), 0, round(low / n, 4))
+)
+
+trainDF = left_join(trainDF, manager, by='manager_id')
+trainDF = left_join(trainDF, building, by='building_id')
+
+
+#add dummy columns for different features
+features$dining_room <- NA
+features[str_detect(features$feature, 'dining room'),]$dining_room <- 1
+
+features$pre_war <- NA
+features[str_detect(features$feature, 'pre-war'),]$pre_war <- 1
+
+features$laundry_in_building <- NA
+features[str_detect(features$feature, 'laundry in building'),]$laundry_in_building <- 1
+
+features$dishwasher <- NA
+features[str_detect(features$feature, 'dishwasher'),]$dishwasher <- 1
+
+features$hardwood_floors <- NA
+features[str_detect(features$feature, 'hardwood floors'),]$hardwood_floors <- 1
+
+features$dogs <- NA
+features[str_detect(features$feature, 'dogs allowed'),]$dogs <- 1
+
+features$cats <- NA
+features[str_detect(features$feature, 'cats allowed'),]$cats <- 1
+
+features$doorman <- NA
+features[str_detect(features$feature, 'doorman'),]$doorman <- 1
+
+features$elevator <- NA
+features[str_detect(features$feature, 'elevator'),]$elevator <- 1
+
+features$no_fee <- NA
+features[str_detect(features$feature, 'no fee'),]$no_fee <- 1
+
+features$fitness_center <- NA
+features[str_detect(features$feature, 'fitness center'),]$fitness_center <- 1
+
+features$laundry_in_unit <- NA
+features[str_detect(features$feature, 'laundry in unit'),]$laundry_in_unit <- 1
+
+features$loft <- NA
+features[str_detect(features$feature, 'loft'),]$loft <- 1
+
+features$fireplace <- NA
+features[str_detect(features$feature, 'fireplace'),]$fireplace <- 1
+
+features$roof_deck <- NA
+features[str_detect(features$feature, 'roof deck'),]$roof_deck <- 1
+
+features$outdoor_space <- NA
+features[str_detect(features$feature, 'outdoor space'),]$outdoor_space <- 1
+
+features$high_speed_internet <- NA
+features[str_detect(features$feature, 'high speed internet'),]$high_speed_internet <- 1
+
+features$balcony <- NA
+features[str_detect(features$feature, 'balcony'),]$balcony <- 1
+
+features$swimming_pool <- NA
+features[str_detect(features$feature, 'swimming pool'),]$swimming_pool <- 1
+
+features$garden_or_patio <- NA
+features[str_detect(features$feature, 'garden/patio'),]$garden_or_patio <- 1
+
+features$wheelchair_access <- NA
+features[str_detect(features$feature, 'wheelchair access'),]$wheelchair_access <- 1
+
+features$common_outdoor_space <- NA
+features[str_detect(features$feature, 'common outdoor space'),]$common_outdoor_space <- 1
+
+features[is.na(features)] <- 0
+
+cols = colnames(features)[3:length(colnames(features))]
+new_features = aggregate(features[cols], by=features['aptID'], FUN=max)
+apartments_features <- left_join(trainDF, new_features, by = 'aptID')
+apartments_features[, 22:54][is.na(apartments_features[, 22:54])] <- 0
+
+saveRDS(apartments_features, 'D:/train-v5.rds')
+write.csv(apartments_features, 'D:/train-v5.csv', row.names=F)
+
+# saveRDS(features, 'D:/features-v2.rds')
+# write.csv(features, 'D:/features-v2.csv', row.names=F)
+#
+# saveRDS(photos, 'D:/photos-v2.rds')
+# write.csv(photos, 'D:/photos-v2.csv', row.names=F)
